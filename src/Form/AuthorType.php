9c626a584204f586ea0f03c6313d097e68af3402
Clemwar <clemwar@gmail.com>
2019-11-27 16:42:02 +0100

    modifs form

--- src/Form/AuthorType.php
+++ src/Form/AuthorType.php
@@ -4,7 +4,10 @@ namespace App\Form;
 
 use App\Entity\Author;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,11 +16,25 @@ class AuthorType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name')
-            ->add('firstName')
-            ->add('birthDate')
-            ->add('deathDate')
-            ->add('biography')
+            ->add('name', TextType::class ,[
+                'label' => 'Nom'
+            ])
+            ->add('firstName', TextType::class ,[
+                'label' => 'Prénom'
+            ])
+            ->add('birthDate', DateType::class, [
+                'years' => range(date('Y'), date('Y') - 500),
+                'label' => 'Date de naissance'
+            ])
+            ->add('deathDate', DateType::class, [
+                'years' => range(date('Y'), date('Y') - 500),
+                'required'=>false,
+                'label' => 'Date de mort'
+            ])
+            ->add('biography', TextareaType::class,[
+                'required'=>false,
+                'label' => 'Biographie'
+            ])
             ->add('save', SubmitType::class)
         ;
     }
--- src/Form/BookType.php
+++ src/Form/BookType.php
@@ -6,7 +6,10 @@ use App\Entity\Author;
 use App\Entity\Book;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -15,13 +18,23 @@ class BookType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('title')
-            ->add('nbPages')
-            ->add('style')
-            ->add('inStock')
+            ->add('title', TextType::class ,[
+                'label' => 'Titre'
+            ])
+            ->add('nbPages', IntegerType::class,[
+                'label' => 'Nombre de pages'
+            ])
+            ->add('style', TextType::class ,[
+                'label' => 'Genre'
+            ])
+            ->add('inStock', CheckboxType::class ,[
+                'label' => 'En stock'
+            ])
             ->add('author', EntityType::class, [
                 'class'   => Author::class,
-                'choice_label' => 'name'])
+                'choice_label' => 'name',
+                'label' => 'Auteur'
+            ])
             ->add('save', SubmitType::class)
         ;
     }
